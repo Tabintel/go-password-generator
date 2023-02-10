e68c06d6602105bb6970574c2281da8cffffd03d
Ekemini Samuel <80429893+Tabintel@users.noreply.github.com>
2023-02-10 14:05:44 +0100

    Add files via upload

---
+++ pass-gen/pass.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+
+const (
+	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialBytes = "!@#$%^&*()_+-=[]{}\\|;':\",.<>/?`~"
+	numBytes = "0123456789"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func generatePassword(length int, includeNumber bool, includeSpecial bool) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var password []byte
+	var charSource string
+
+	if includeNumber {
+		charSource += "0123456789"
+	}
+	if includeSpecial {
+		charSource += "!@#$%^&*()_+=-"
+	}
+	charSource += charset
+
+	for i := 0; i < length; i++ {
+		randNum := rand.Intn(len(charSource))
+		password = append(password, charSource[randNum])
+	}
+	return string(password)
+}
+
+func main() {
+	password := generatePassword(12, true, true)
+	fmt.Println("Random password:", password)
+}
